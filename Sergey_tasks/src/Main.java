1860fa14cf2148208411606d995a59e29d91a10d
Irrisorr <zakharkevichg@gmail.com>
2024-04-01 22:15:09 +0200

    created calc size file func

--- Sergey_tasks/src/Main.java
+++ Sergey_tasks/src/Main.java
@@ -1,21 +1,12 @@
 // 
 
 import java.io.*;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-
-//        try (BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-//             File file = new File(bf.readLine());
-//             FileInputStream fi = new FileInputStream(bf.readLine());
-//             FileOutputStream fo = new FileOutputStream(bf.readLine())){
-//
-//            fi.
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
 
         try {
             BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -23,8 +14,29 @@ public class Main {
             FileInputStream fis = new FileInputStream(file);
             FileOutputStream fos = new FileOutputStream(bf.readLine());
 
-            String fileNameToFO = String.format("Name of input file: %s", file.getName()); 
+//            String fileType;
+//            byte[] bytes = fis.readAllBytes();
+//            boolean hasBinary = IntStream.range(0, bytes.length)
+//                    .map(i -> Byte.toUnsignedInt(bytes[i]))
+//                    .anyMatch(b -> b > 127);
+//
+//            if (hasBinary) {
+//                fileType = "Binary";
+//            } else {
+//                fileType = "Text";
+//            }
+
+            String fileNameToFO =
+                    String.format("Name of input file: %s\nFile size: %d (bytes)\nFile type: %s",
+                            file.getName(), file.length(), fileType);
+
+
             fos.write(fileNameToFO.getBytes());
+
+
+            bf.close();
+            fis.close();
+            fos.close();
 
         } catch (Exception e) {
             e.printStackTrace();
