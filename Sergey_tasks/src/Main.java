4761909af691bbd29f91e8ac6583f6a8e4752f65
Irrisorr <zakharkevichg@gmail.com>
2024-04-01 19:48:30 +0200

    created func for write fileName into FO

--- Sergey_tasks/src/Main.java
+++ Sergey_tasks/src/Main.java
@@ -1,7 +1,34 @@
+// 
 
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
+
+//        try (BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//             File file = new File(bf.readLine());
+//             FileInputStream fi = new FileInputStream(bf.readLine());
+//             FileOutputStream fo = new FileOutputStream(bf.readLine())){
+//
+//            fi.
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+
+        try {
+            BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+            File file = new File(bf.readLine());
+            FileInputStream fis = new FileInputStream(file);
+            FileOutputStream fos = new FileOutputStream(bf.readLine());
+
+            String fileNameToFO = String.format("Name of input file: %s", file.getName()); 
+            fos.write(fileNameToFO.getBytes());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
