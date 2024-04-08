4eb1057e6facc8bac0257c3bdd4a40fbda118791
Irrisorr <zakharkevichg@gmail.com>
2024-04-08 21:39:13 +0200

    Modify func for determine file type

--- Sergey_tasks/src/Main.java
+++ Sergey_tasks/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
@@ -10,25 +11,17 @@ public class Main {
             FileOutputStream fos = new FileOutputStream(bf.readLine());
 
             String fileType;
+            String fileEncoding;
             byte[] buffer = new byte[4096];
 
             int bytesRead = fis.read(buffer);
             if (bytesRead == -1) {
                 fileType = "Empty file";
             } else {
-                boolean isText = true;
-                for (int i = 0; i < bytesRead - 1; i++) {
-                    byte b1 = buffer[i];
-                    byte b2 = buffer[i + 1];
-
-                    if ((b1 == 0 && b2 != 0) || (b1 != 0 && b2 == 0)) {
-                        isText = false;
-                        break;
-                    }
-                }
+                boolean isText = isTextFile(buffer, bytesRead);
 
                 if (isText) {
-                    fileType = "Text file (Unicode)";
+                    fileType = "Text file";
                 } else {
                     fileType = "Binary file";
                 }
@@ -38,9 +31,7 @@ public class Main {
                     String.format("Name of input file: %s\nFile size: %d (bytes)\nFile type: %s",
                             file.getName(), file.length(), fileType);
 
-
             fos.write(fileNameToFO.getBytes());
-
 
             bf.close();
             fis.close();
@@ -50,4 +41,18 @@ public class Main {
             e.printStackTrace();
         }
     }
+
+    private static boolean isTextFile(byte[] buffer, int bytesRead) {
+        for (int i = 0; i < bytesRead - 1; i++) {
+            byte b1 = buffer[i];
+            System.out.println(b1);
+            byte b2 = buffer[i + 1];
+
+            if (b1 == 0 && b2 == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
